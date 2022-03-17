fdd82090c6f6634c8a5e2d1f2aa0a3c342025290
Satynand  Yadav <satyanand.yadav.19.1@mountblue.tech>
2022-03-17 11:39:33 +0530

    new change in reduce

--- reduce.js
+++ reduce.js
@@ -1,11 +1,24 @@
 function reduce(elements, cb, startingValue) {
     if (elements) {
+        
+        if(startingValue ) {
+           
+        
 
+        for (let counter = 0; counter < elements.length; counter++) {
+            startingValue = cb(startingValue, elements[counter],counter,elements);
+
+        }
+        return startingValue;
+    }
+    else {
+           startingValue =elements[0];
         for (let counter = 1; counter < elements.length; counter++) {
             startingValue = cb(startingValue, elements[counter],counter,elements);
 
         }
         return startingValue;
+    }
     }
     else {
         return 0;
@@ -19,7 +32,7 @@ function sumofAllElement(sum, eachItem,index,array) {
 
 function displayValue(sumResult) {
 
-    !sumResult ? console.log("Invalid Input") : console.log("Sum of all the element of array : " + sumResult);
+    !sumResult ? console.log("Invalid Input") : console.log("Sum of all the element of array : " , sumResult);
 
 }
 
--- testReduce.js
+++ testReduce.js
@@ -1,15 +1,15 @@
 let testObject = require('./reduce');
 
-const evenNumberElements = [2, 4, 6, 8, 10, 12];
+const evenNumberElements = [1,2,3];
 const emptyElements = [];
 const oddNumberElements = [1, 3, 5, 7, 9, 99];
 const primeElements = [2,3,5,7,11,13,17,19];
 const nullElements = null;
 const undefinedElements = undefined;
 
-testObject.displayValue(testObject.reduce(evenNumberElements, testObject.sumofAllElement, evenNumberElements[0]));
-testObject.displayValue(testObject.reduce(emptyElements, testObject.sumofAllElement, emptyElements[0])); // return zero
-testObject.displayValue(testObject.reduce(oddNumberElements, testObject.sumofAllElement, oddNumberElements[0])); 
-testObject.displayValue(testObject.reduce(primeElements, testObject.sumofAllElement, primeElements[0])); 
+testObject.displayValue(testObject.reduce(evenNumberElements, testObject.sumofAllElement,5));
+testObject.displayValue(testObject.reduce(emptyElements, testObject.sumofAllElement,{})); // return zero
+testObject.displayValue(testObject.reduce(oddNumberElements, testObject.sumofAllElement, 0)); 
+testObject.displayValue(testObject.reduce(primeElements, testObject.sumofAllElement, 0)); 
 testObject.displayValue(testObject.reduce(nullElements, testObject.sumofAllElement, null));  //return zero
 testObject.displayValue(testObject.reduce(undefinedElements, testObject.sumofAllElement, undefined)); // return zero
